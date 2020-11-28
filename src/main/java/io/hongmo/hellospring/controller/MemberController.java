17ea10d8058e3dcebdd86d170a543b19a91ffc7a
hongmo sung <tesung30@naver.com>
2020-11-28 23:01:25 +0900

    회원 웹 기능 - 조회

--- src/main/java/io/hongmo/hellospring/controller/MemberController.java
+++ src/main/java/io/hongmo/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package io.hongmo.hellospring.controller;
 
+import io.hongmo.hellospring.domain.Member;
 import io.hongmo.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,26 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createMembersForm() {
+        return "/members/createMembersForm";
+    }
+
+    @PostMapping("/members/new")
+    public String createForm(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String members(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
